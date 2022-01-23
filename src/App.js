f0ed2974e6e2e17e4e36a05da31a40bf52e5fa7e
tangentdigitalagency <david@tangentdigitalagency.com>
2022-01-22 23:29:59 -0700

    additions

--- src/App.js
+++ src/App.js
@@ -1,25 +1,53 @@
-import logo from './logo.svg';
-import './App.css';
+import React, { Component } from 'react';
+import { Route, Switch } from 'react-router';
+import { BrowserRouter } from 'react-router-dom';
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+
+import NavBar from './Navbar';
+import LandingPage from './LandingPage';
+class App extends Component {
+
+  state = {
+    mainRoute: '/',
+
+    healthRoutes: [
+      '/'
+    ],
+
+    medicareRoutes: [
+      '/'
+    ],
+
+    healthPostData: {
+
+    },
+
+    medicarePostData: {
+
+    }
+  }
+
+  changeRoute = () => {
+    this.setState({
+      route: ''
+    });
+  };
+
+
+  render() {
+    return (
+      <BrowserRouter>
+        <div>
+      <NavBar />
+          <Switch>
+            <Route path='/' exact>
+            <LandingPage />
+              </Route>
+          </Switch>
+        </div>
+      </BrowserRouter>
+    )
+  }
 }
 
 export default App;
